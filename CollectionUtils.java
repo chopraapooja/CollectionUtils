f17fca58fbb28051458323aebf84327f6c25806a
unknown <poojar@INpoojar.corporate.thoughtworks.com>
2015-02-28 13:17:40 +0530

    Implemented ListMapper in NumberMapper

--- CollectionUtils.java
+++ CollectionUtils.java
@@ -1,19 +1,19 @@
 import java.util.List;
 import java.util.ArrayList;
 
-// interface ListMapper<E> {
-// 	E mapper(E ele);
-// }
+interface ListMapper <RT,E>{
+	public RT mapper(E ele);
+}
 
-// class NumberMapper implements ListMapper{
-// 	static Integer mapper(Integer num) {
-// 		return num;
-// 	}
-// }
+class NumberMapper implements ListMapper<Integer, Integer>{
+	public Integer mapper(Integer num) {
+		return num;
+	}
+}
 
 class CollectionUtils {
 	
-	static <E> List<E> map(List<E> list) {
+	static <E> List<E> map(List<E> list, ListMapper m) {
 		return list;
 	}
 	// static List<RT> mapper(List<E> list, ListMapper mapper) {
@@ -23,10 +23,9 @@ class CollectionUtils {
 	public static void main(String[] args) {
 		List<Integer> numbers = new ArrayList<Integer>();
 		numbers.add(1);
+		ListMapper mapper = new NumberMapper();
 
-		System.out.println(numbers.toString());
-		System.out.println(CollectionUtils.map(numbers));
-		// ListMapper lm = new NumberMapper();
-		// numbers.map(numbers, lm); 
+		System.out.println(numbers.toString());	
+		System.out.println(CollectionUtils.map(numbers, mapper));
 	}
 }
